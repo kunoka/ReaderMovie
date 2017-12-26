6b1b2882fbc966a3326ad0efffcc7cbb6b1231ab
Harry <hengwang@cn.ibm.com>
2017-12-26 08:49:44 +0800

    chapter 7-1 end

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -69,5 +69,19 @@ Page({
     wx.navigateTo({
       url: 'post-detail/post-detail?id='+postId
     })
+  },
+  // onSwiperItemTap: function (event) {
+  //   console.log('onSwiperItemTap')
+  //   var postId = event.currentTarget.dataset.postid;
+  //   wx.navigateTo({
+  //     url: 'post-detail/post-detail?id=' + postId
+  //   })
+  // },
+  onSwiperTap: function (event) {
+    console.log('onSwiperTap')
+    var postId = event.target.dataset.postid;
+    wx.navigateTo({
+      url: 'post-detail/post-detail?id=' + postId
+    })
   }
 })
