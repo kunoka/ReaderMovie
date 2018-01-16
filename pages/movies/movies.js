be13e05095628dda67172e9f8c08b8a8a353d00e
Harry <hengwang@cn.ibm.com>
2018-01-16 16:07:42 +0800

    7-13 end

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -5,85 +5,107 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
-    var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters";
-    var comingSoonUrl = app.globalData.doubanBase  + "/v2/movie/coming_soon";
+
+    var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters?start=0&count=3";
+    var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon";
     var top250Url = app.globalData.doubanBase + "/v2/movie/top250";
-   
+
     this.getMovieListData(inTheatersUrl);
-    this.getMovieListData(top250Url);
-    this.getMovieListData(top250Url);
+    // this.getMovieListData(top250Url);
+    // this.getMovieListData(top250Url);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  getMovieListData: function(url){
+  getMovieListData: function (url) {
+    var that = this;
     wx.request({
       url: url,
       method: 'GET',
       header: {
-        'Content-Type':'xml'
+        'Content-Type': 'xml'
       },
-      success: function(res){
-        console.log('success')
-        console.log(res);
+      success: function (res) {
+        // console.log('success');
+        // console.log(res);
+        that.getMovieData(res.data.subjects);
       },
-      fail: function(res){
+      fail: function (res) {
         console.log('fail')
       }
     })
   },
+  getMovieData: function (subjects) {
+    console.log(subjects)
+    var movies = [];
+    for (var i = 0; i < subjects.length; i++) {
+      var movie = subjects[i];
+      var temp = {
+        title: movie.title.length >=6? movie.title.substr(0,6)+'...' : movie.title,
+        coverageUrl: movie.images.large.replace('img7','img3'),
+        average: movie.rating.average,
+        movieId: movie.id
+      }
+      console.log(temp)
+      movies.push(temp)
+    }
+   
+  
+    this.setData({
+      movies: movies
+    })
+  },
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
